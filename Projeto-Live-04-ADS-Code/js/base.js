f2b77f1a63028b0edeae4071973cd579d69f51cf
Marcelo Velasque <perfil3.0@hotmail.com>
2024-06-27 22:31:23 -0300

    Exercicio Cap09 para entregar e outros projetinhos paralelos

---
+++ Projeto-Live-04-ADS-Code/js/base.js
@@ -0,0 +1,13 @@
+// console do browser
+console.log('Olá eu sou o Browser');
+console.log('Olá novamente');
+
+let nomeUsuario = 'Marcelo Velasque'
+
+let idUsuario = 789456
+let logado = true
+let dataNascimento;
+
+console.log(nomeUsuario, idUsuario, logado, dataNascimento)
+
+
